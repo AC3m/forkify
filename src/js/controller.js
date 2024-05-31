490d026368a90bfa0775d37bec8ba254fac9edd9
Artur Cempura <artur.cempura@grandparade.co.uk>
2024-05-31 21:42:16 +0200

    Fixed controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -72,7 +72,7 @@ const controlServings = function (newServings) {
   recipeView.update(model.state.recipe);
 };
 
-controlAddBookmark = function () {
+const controlAddBookmark = function () {
   // 1) Add/remove bookmark
   if (!model.state.recipe.bookmarked) {
     model.addBookmark(model.state.recipe);
